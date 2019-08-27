7cf6606da2c3c69860ad2a6d0fadc18dae2c6d29
Stefan <me@Stefan.com>
2019-08-27 20:56:00 +0300

    Adding Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+
+const rootState = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+	<Provider store = {rootState}>
+	    <BrowserRouter>
+		    <App />
+	    </BrowserRouter>
+    </Provider> 
 );
 
 ReactDOM.render( app, document.getElementById( 'root' ) );
