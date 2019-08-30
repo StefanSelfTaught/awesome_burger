c3306a34c7ed99724649c655cfc08ab98c2a8aa0
Stefan <me@Stefan.com>
2019-08-30 16:33:03 +0300

    Adding Order Reducer

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,42 +6,44 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
 
-    checkoutCancelledHandler = () => {
-        this.props.history.goBack();
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  };
+
+  render() {
+    let checkoutSummary;
+    if (this.props.history.location['pathname'] === '/checkout') {
+      checkoutSummary = (
+        <CheckoutSummary
+          ingredients={this.props.ings}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+      );
+    } else {
+      checkoutSummary = null;
     }
 
-    checkoutContinuedHandler = () => {
-        this.props.history.replace( '/checkout/contact-data' );
-    }
-
-    render () {
-        
-        let checkoutSummary;
-        if(this.props.history.location['pathname'] === "/checkout"){
-            checkoutSummary = <CheckoutSummary 
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-        } else {
-            checkoutSummary = null;
-        }
-
-        return (
-            <div>
-                <Route 
-                    path={this.props.match.path + '/contact-data'} 
-                    component={ContactData}/>
-                {checkoutSummary}  
-            </div>
-        );
-    }
+    return (
+      <div>
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData}
+        />
+        {checkoutSummary}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        ings: state.ingredients
-    }
-}
+  return {
+    ings: state.burgerBuilder.ingredients
+  };
+};
 
 export default connect(mapStateToProps)(Checkout);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/oder';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+});
+
 const rootState = createStore(
-  burgerBuilderReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
