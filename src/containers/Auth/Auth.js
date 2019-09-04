b6dadb09da789366951c378642f103f1af7eaa08
Stefan <me@Stefan.com>
2019-09-04 19:39:56 +0300

    Protected Resources and Logout

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Auth extends Component {
   state = {
@@ -102,7 +103,6 @@ class Auth extends Component {
       return { isSignup: !prevState.isSignup };
     });
   };
-
   render() {
     const formElementArray = [];
     for (let key in this.state.controls) {
@@ -125,20 +125,24 @@ class Auth extends Component {
       />
     ));
 
-    if(this.props.loading) {
-      form = <Spinner />
+    if (this.props.loading) {
+      form = <Spinner />;
     }
 
     let errorMessage = null;
 
-    if(this.props.error) {
-      errorMessage = (
-          <p>{this.props.error.message}</p>
-        )
+    if (this.props.error) {
+      errorMessage = <p>{this.props.error.message}</p>;
+    }
+
+    let authRedirect = null;
+    if (this.props.isAuthenticated) {
+      authRedirect = <Redirect to='/' />;
     }
 
     return (
       <div className={classes.Auth}>
+        {authRedirect}
         {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
@@ -155,9 +159,10 @@ class Auth extends Component {
 const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
-    error: state.auth.error
-  }
-}
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
